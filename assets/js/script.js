01a92a0dca7bd48c923a64f9cfaed1d3f4bc5bb0
Mary Prince <mleeherr@gmail.com>
2022-07-01 14:48:16 -0400

    linked js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let maryCount = 60
+let travisCount = 0
+let jungCount = 0
+let donnieCount = 0
+
+let rows = document.getElementsByClassName('row')
+
+
+// I started just going with this and I figure I better plan instead of just like - going for it.
+//Instead of hard-coding rows and scores, I want to create objects that store name and score in local storage.
+let lotIt = () =>{
+console.log("hi!!");}
+logIt();
+
+let addNewPlayer = () =>{
+    document.getElementById("addPlayer").onclick = function(){
+        console.log('Hello world');
+    }
+}
+
+addNewPlayer();
--- assets/script.js
+++
@@ -1,9 +0,0 @@
-let maryCount = 60
-let travisCount = 0
-let jungCount = 0
-let donnieCount = 0
-
-let rows = document.getElementsByClassName('row')
-
-
-// I started just going with this and I figure I better plan instead of just like - going for it.
