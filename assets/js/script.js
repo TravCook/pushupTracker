520ed6fbb0c726e365fd676ef0e0aa3864d9ffb8
Mary Prince <mleeherr@gmail.com>
2022-07-01 15:05:26 -0400

    function to add to the table

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,17 @@ let rows = document.getElementsByClassName("row");
 let addNewPlayer = () => {
   document.getElementById("addPlayer").onclick = function () {
     console.log("button was clicked");
-    var table = document.getElementById("playerTable");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
+    //we made a variable called table to grab the table from the html, and some variables that insert rows and cells into that table
+    let table = document.getElementById("playerTable");
+    let row = table.insertRow(0);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    cell1.innerHTML = "Player Name";
+    cell2.innerHTML = "0";
+    cell3.innerHTML = "<button>add Push Up</div>";
   };
 };
 addNewPlayer();
+
+//I would like to add a button for each cell to increase or decrease the score; maybe I shoule make an object?!
