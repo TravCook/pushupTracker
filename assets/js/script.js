68609b64ec7984aa47531c81b69930167bfa250f
Mary Prince <mleeherr@gmail.com>
2022-07-01 14:57:24 -0400

    makin a button make some stuff!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
-let maryCount = 60
-let travisCount = 0
-let jungCount = 0
-let donnieCount = 0
+let maryCount = 60;
+let travisCount = 0;
+let jungCount = 0;
+let donnieCount = 0;
 
-let rows = document.getElementsByClassName('row')
-
+let rows = document.getElementsByClassName("row");
 
 // I started just going with this and I figure I better plan instead of just like - going for it.
 //Instead of hard-coding rows and scores, I want to create objects that store name and score in local storage.
-let lotIt = () =>{
-console.log("hi!!");}
-logIt();
 
-let addNewPlayer = () =>{
-    document.getElementById("addPlayer").onclick = function(){
-        console.log('Hello world');
-    }
-}
-
+//a function to create a new column and row for each new player.
+let addNewPlayer = () => {
+  document.getElementById("addPlayer").onclick = function () {
+    console.log("button was clicked");
+    var table = document.getElementById("playerTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+  };
+};
 addNewPlayer();
